4867330557a1d08a2688565062e41ad7d85d9350
Your Name <you@example.com>
2016-10-07 16:31:07 +0000

    Run lein new app

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gof
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/gof/core_test.clj
@@ -0,0 +1,7 @@
+(ns gof.core-test
+  (:require [clojure.test :refer :all]
+            [gof.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
