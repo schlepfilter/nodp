7546f5ff2269546fdf9fa728aae9599f210def45
Your Name <you@example.com>
2016-10-09 12:24:42 +0000

    Apply free-point style

--- src/hfdp/adapter.clj
+++ src/hfdp/adapter.clj
@@ -1,15 +1,13 @@
 (ns hfdp.adapter)
 
-(defn- turkey-quack
-  []
-  (println "Gobble gobble"))
+(def turkey-quack
+  (partial println "Gobble gobble"))
 
 (def duck-quack
   turkey-quack)
 
-(defn- turkey-fly
-  []
-  (println "I'm flying a short distance"))
+(def turkey-fly
+  (partial println "I'm flying a short distance"))
 
 (defn duck-fly
   []
