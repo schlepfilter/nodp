02bc110a1e37d5db8f433b3c7274e5888c4bc5bb
Your Name <you@example.com>
2016-10-09 08:33:35 +0000

    Fly

--- src/hfdp/adopter.clj
+++ src/hfdp/adopter.clj
@@ -2,8 +2,20 @@
 
 (defmulti perform identity)
 
-(defmethod perform [:duck :quack]
-  [& _]
-  (println "Gobble gobble"))
+(defmacro defperform
+  [object action expr]
+  `(defmethod perform [~object ~action]
+     [& _#]
+     ~expr))
+
+(defperform :duck :quack (println "Gobble gobble"))
+
+(defn- fly
+  []
+  (dotimes [_ 5] (println "I'm flying a short distance")))
+
+(defperform :duck :fly (fly))
 
 (perform [:duck :quack])
+
+(perform [:duck :fly])
