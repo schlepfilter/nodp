742cb1c8195b6420e9f3e86d546a5266fbbe5705
Your Name <you@example.com>
2016-10-10 19:43:37 +0000

    Extract get-ingredients

--- src/hfdp/factory.clj
+++ src/hfdp/factory.clj
@@ -1,23 +1,6 @@
 (ns hfdp.factory
   (:require [riddley.walk :as riddley]))
 
-(defmulti get-regional-ingredient :region)
-
-(defmethod get-regional-ingredient :ny
-  [_]
-  {:cheese    "Reggiano Cheese"
-   :dough     "Thin Crust Dough"
-   :clams     "Fresh Clams from Long Island Sound"
-   :pepperoni "Sliced Pepperoni"                            ;
-   :sauce     "Marinara Sauce"
-   :vegies    #{"Garlic" "Onion" "Mashroom" "Red Pepper"}})
-
-(defmulti get-kind-ingredients :kind)
-
-(defmethod get-kind-ingredients :cheese
-  [_]
-  #{:dough :sauce :cheese})
-
 (defn quote-form
   [form]
   `'~form)
@@ -38,12 +21,31 @@
      (partial apply (functionize ~operator))
      (juxt ~@fs)))
 
+(defmulti get-regional-ingredient :region)
+
+(defmethod get-regional-ingredient :ny
+  [_]
+  {:cheese    "Reggiano Cheese"
+   :dough     "Thin Crust Dough"
+   :clams     "Fresh Clams from Long Island Sound"
+   :pepperoni "Sliced Pepperoni"                            ;
+   :sauce     "Marinara Sauce"
+   :vegies    #{"Garlic" "Onion" "Mashroom" "Red Pepper"}})
+
+(defmulti get-kind-ingredients :kind)
+
+(defmethod get-kind-ingredients :cheese
+  [_]
+  #{:dough :sauce :cheese})
+
+(def get-ingredients
+  (build select-keys
+         get-regional-ingredient
+         get-kind-ingredients))
+
 (def log-pizza
   ;TODO log the name of the pizza
-  (comp (partial println)
-        (build select-keys
-               get-regional-ingredient
-               get-kind-ingredients)))
+  (comp println get-ingredients))
 
 (def operations
   ["box" "cut" "bake" "prepare"])
