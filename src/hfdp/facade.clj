731d1a0e8815fa8f99bf2aeeb68d14b33ec5eed9
Your Name <you@example.com>
2016-10-10 23:34:43 +0000

    Build arguments first

--- src/hfdp/facade.clj
+++ src/hfdp/facade.clj
@@ -2,44 +2,41 @@
   (:require [hfdp.helpers :as helpers]
             [clojure.string :as str]))
 
-(defn- run-command
+(defn- get-arguments
   [device command]
   (if (fn? command)
     (command device)
     (apply (first command) device (rest command))))
 
-(defn- run-commands
+(defn- get-arguments-sequence
   [device & commands]
-  (-> (partial run-command device)
-      (map commands)
-      dorun))
+  (-> (partial get-arguments device)
+      (map commands)))
 
-(defn- print-sentence
+(defn- get-sentence
   [& more]
-  ;TODO decouble side effect
-  (-> (str/join " " more)
-      println))
+  (str/join " " more))
 
 (def turn-on
-  (partial (helpers/flip print-sentence) "on"))
+  (partial (helpers/flip get-sentence) "on"))
 
 (defn- set-dvd
   [amp dvd]
-  (print-sentence amp "setting DVD player to" dvd))
+  (get-sentence amp "setting DVD player to" dvd))
 
 (defn- play
   [dvd film]
-  (print-sentence dvd "playing" film))
+  (get-sentence dvd "playing" film))
 
 (defn watch-film
   [{:keys [amp dvd film]}]
   ;TODO extract a function to run multiple run-commands
-  (run-commands amp
-                turn-on
-                [set-dvd dvd])
-  (run-commands dvd
-                turn-on
-                [play film]))
+  (dorun (map println (get-arguments-sequence amp
+                                              turn-on
+                                              [set-dvd dvd])))
+  (dorun (map println (get-arguments-sequence dvd
+                                              turn-on
+                                              [play film]))))
 
 (watch-film {:amp  "Top-O-Line Amplifier"
              :dvd  "Top-O-Line DVD Player"
