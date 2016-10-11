db2918c59c2901b0f6bcad78ec60da6ed107fc58
Your Name <you@example.com>
2016-10-11 10:46:36 +0000

    Specify commands with keywords only

--- src/hfdp/facade.clj
+++ src/hfdp/facade.clj
@@ -8,6 +8,11 @@
    :turn-off "off"
    :turn-on  "on"})
 
+(def theater
+  {:amp  "Top-O-Line Amplifier"
+   :dvd  "Top-O-Line DVD Player"
+   :film "Raiders of the Lost Ark"})
+
 (defn- get-sentence
   [verb & more]
   (->> (conj (rest more) (verb actions) (first more))
@@ -15,9 +20,12 @@
 
 (defn- get-action
   [device command]
-  (if (keyword? command)
-    (get-sentence command device)
-    (get-sentence (first command) device (second command))))
+  (let [device-name (device theater)]
+    (if (keyword? command)
+     (get-sentence command device-name)
+     (get-sentence (first command)
+                   device-name
+                   ((second command) theater)))))
 
 (defn- get-device-actions
   [[device & commands]]
@@ -36,28 +44,27 @@
   (comp helpers/printall
         get-arguments))
 
+(def watch-device-commands
+  [[:amp
+    :turn-on
+    [:set-dvd :dvd]]
+   [:dvd
+    :turn-on
+    [:play :film]]])
+
 (defn watch-film
-  [{:keys [amp dvd film]}]
-  (-> {:device-commands [[amp
-                          :turn-on
-                          [:set-dvd dvd]]
-                         [dvd
-                          :turn-on
-                          [:play film]]]
-       :description     "Get ready to watch a movie..."}
+  [theater]
+  (-> {:description     "Get ready to watch a movie..."
+       :device-commands watch-device-commands}
       print-arguments))
 
-(defn end-film
-  [{:keys [amp dvd film]}]
-  (-> {:device-commands [[dvd
-                          :turn-off]]
-       :description     "Shutting movie theater down..."}
-      print-arguments))
-
-(def theater {:amp  "Top-O-Line Amplifier"
-              :dvd  "Top-O-Line DVD Player"
-              :film "Raiders of the Lost Ark"})
+;(defn end-film
+;  [{:keys [amp dvd film]}]
+;  (-> {:device-commands [[dvd
+;                          :turn-off]]
+;       :description     "Shutting movie theater down..."}
+;      print-arguments))
 
 (watch-film theater)
 
-(end-film theater)
+;(end-film theater)
