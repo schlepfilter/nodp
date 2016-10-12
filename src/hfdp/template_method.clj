21dd7e3c6cf6c82722762f4c9402415de8eeb19c
Your Name <you@example.com>
2016-10-12 21:30:47 +0000

    Prepare

--- src/hfdp/template_method.clj
+++ src/hfdp/template_method.clj
@@ -9,12 +9,20 @@
   [_]
   (println "Dripping Coffee through filter"))
 
+(def pour
+  (partial println "Pouring into cup"))
+
+(defmulti add-condiments identity)
+
+(defmethod add-condiments :coffee
+  [_]
+  (println "Adding Sugar and Milk"))
+
 (defn prepare
   [kind]
   (boil)
   (brew kind)
-  ;(pour)
-  ;(add-condiments kind)
-  )
+  (pour)
+  (add-condiments kind))
 
 (prepare :coffee)
