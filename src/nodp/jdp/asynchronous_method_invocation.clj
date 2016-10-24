b6a42613ec8c7b646637ea08629f26c9b408adef
Your Name <you@example.com>
2016-10-24 07:49:58 +0000

    Get multiple results

--- src/nodp/jdp/asynchronous_method_invocation.clj
+++ src/nodp/jdp/asynchronous_method_invocation.clj
@@ -1,4 +1,5 @@
-(ns nodp.jdp.asynchronous-method-invocation)
+(ns nodp.jdp.asynchronous-method-invocation
+  (:require [nodp.helpers :as helpers]))
 
 (defn- get-thread-name
   []
@@ -8,17 +9,32 @@
 (def prefix-with-thread-name
   (partial str "[" (get-thread-name) "] - "))
 
-(def log
-  (comp println prefix-with-thread-name))
+(def get-completion
+  (partial str "Task completed with: "))
+
+(def get-prefixed-completion
+  (comp prefix-with-thread-name get-completion))
 
 (defn get-value
   [{:keys [value delay callback-prefix]}]
   (future
     (Thread/sleep delay)
-    (log (str "Task completed with: " value))
+    (println (get-prefixed-completion value))
     (if callback-prefix
-      (log (str callback-prefix ": " value)))
+      (println (get-prefixed-completion callback-prefix ": " value)))
     value))
 
-(log (str "Result 1: " @(get-value {:value 10
-                                    :delay 500})))
+(defn- get-result
+  [n m]
+  (str "Result " n ": " @(get-value m)))
+
+(def get-prefixed-result
+  (comp prefix-with-thread-name get-result))
+
+(def get-results
+  (partial map get-prefixed-result (range)))
+
+(helpers/printall (get-results [{:value 10
+                                 :delay 500}
+                                {:value "test"
+                                 :delay 300}]))
