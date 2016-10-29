ff470cd224e51eddc1077cbb3a5cea890146ec53
Your Name <you@example.com>
2016-10-29 23:41:56 +0000

    Keep contact and check credit

---
+++ src/nodp/eip/routing_slip.clj
@@ -0,0 +1,29 @@
+(ns nodp.eip.routing-slip
+  (:require [nodp.helpers :as helpers]))
+
+(def customer
+  {:name    "ABC Inc."
+   :tax-id  "123-45-6789"
+   :contact {:address-1 "123 Main Street"
+             :address-2 "Suite 100"
+             :city      "Boulder"
+             :state     "CO"
+             :zip       "80301"
+             :phone     "303-555-1212"}
+   :service "99-1203"})
+
+(def keep-contact
+  (comp (partial str "handling register customer to keep contact information: ")
+        :contact))
+
+(def check-credit
+  (comp (partial str "handling register customer to perform credit check: ")
+        :tax-id))
+
+(def handle-customer
+  (helpers/build vector keep-contact check-credit))
+
+(def printall
+  (comp helpers/printall handle-customer))
+
+(printall customer)
