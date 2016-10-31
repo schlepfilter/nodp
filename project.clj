f181afb994a255b95f065dae388189632624feb1
Your Name <you@example.com>
2016-10-31 23:58:39 +0000

    Taste stew

--- project.clj
+++ project.clj
@@ -10,6 +10,7 @@
                  [org.clojure/math.combinatorics "0.1.3"]
                  [org.flatland/ordered "1.5.4"]
                  [riddley "0.1.12"]
+                 [com.rpl/specter "0.13.0"]
                  [thi.ng/geom "0.0.908"]]
   :main ^:skip-aot nodp.core
   :target-path "target/%s"
--- src/nodp/jdp/private_class_data.clj
+++ src/nodp/jdp/private_class_data.clj
@@ -1,6 +1,8 @@
 (ns nodp.jdp.private-class-data
   (:require [clojure.string :as str]
-            [inflections.core :as inflections]))
+            [inflections.core :as inflections]
+            [com.rpl.specter :as specter]
+            [nodp.helpers :as helpers]))
 
 (def stew {:potato 1
            :carrot 2
@@ -28,4 +30,19 @@
         get-and
         (partial map get-ingredient)))
 
+
+(specter/transform specter/MAP-VALS dec stew)
+
+(defn- taste
+  [food]
+  (specter/transform specter/MAP-VALS dec food))
+
 (mix stew)
+
+(def taste-mix
+  (juxt (constantly "Tasting the stew")
+        (comp mix taste)))
+
+(-> stew
+    taste-mix
+    helpers/printall)
