3c8640cacce647066259c08c977c04dce23c2402
Your Name <you@example.com>
2016-10-31 22:08:40 +0000

    Add todo

--- src/nodp/jdp/double_dispatch.clj
+++ src/nodp/jdp/double_dispatch.clj
@@ -4,20 +4,24 @@
             [thi.ng.geom.rect :as rect]))
 
 (def asteroid
-  {:name     "FlamingAsteroid"
-   :position (rect/rect [0 0] [5 5])})
+  {:flaming  true
+   :position (rect/rect [0 0] [5 5])
+   :subject  :teroid})
 
 (def mir
-  {:name     "SpaceStationMir"
-   :position (rect/rect [1 1] [2 2])})
+  {:flaming  false
+   :position (rect/rect [1 1] [2 2])
+   :subject  :station})
 
 (def meteroid
-  {:name     "Meteroid"
-   :position (rect/rect [10 10] [15 15])})
+  {:flaming  false
+   :position (rect/rect [10 10] [15 15])
+   :subject  :teroid})
 
 (def iss
-  {:name     "SpaceStationIss"
-   :position (rect/rect [12 12] [14 14])})
+  {:flmaing  false
+   :position (rect/rect [12 12] [14 14])
+   :subject  :station})
 
 (def bodies
   #{asteroid mir meteroid iss})
@@ -26,5 +30,12 @@
   (comp
     (partial apply intersect/intersect-rect-rect?)
     (partial map :position)))
+
+;TODO implement collide with predicate dispatch when core.match supports predicate dispatch
+
+;Road Map
+;
+;A good chunk of Maranget's algorithm for pattern matching has been implemented. We would like to flesh out the pattern matching functionality. Once that work is done, we'll move on to predicate dispatch.
+;https://github.com/clojure/core.match/wiki/Overview
 
 (filter intersect? (combo/combinations bodies 2))
