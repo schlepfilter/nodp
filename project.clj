1f37e88e8804ff9afd44e90cb623cfbf272d774f
Your Name <you@example.com>
2016-11-01 22:50:53 +0000

    Add a todo for decorator

--- project.clj
+++ project.clj
@@ -5,12 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clojurewerkz/money "1.9.0"]
+                 [com.rpl/specter "0.13.0"]
                  [funcool/cats "2.0.0"]
                  [inflections "0.12.2"]
                  [org.clojure/math.combinatorics "0.1.3"]
                  [org.flatland/ordered "1.5.4"]
+                 [prismatic/plumbing "0.5.3"]
                  [riddley "0.1.12"]
-                 [com.rpl/specter "0.13.0"]
                  [thi.ng/geom "0.0.908"]]
   :main ^:skip-aot nodp.core
   :target-path "target/%s"
---
+++ src/nodp/jdp/decorator.clj
@@ -0,0 +1,20 @@
+(ns nodp.jdp.decorator
+  (:require [plumbing.map :as map]))
+
+(def troll
+  {:attack "The troll swings at you with a club!"
+   :flee   "The troll shrieks in horror and runs away!"
+   :kind   "simple"
+   :power  10})
+
+(def smart
+  {:attack "It throws a rock at you!"
+   :flee   "It calls for help!"
+   :kind   "smart"
+   :power  20})
+
+(defmulti decorate (comp first vector))
+
+;TODO implement decorate with predicate dispatch when core.match supports predicate dispatch
+
+(map/merge-with-key decorate troll smart)
