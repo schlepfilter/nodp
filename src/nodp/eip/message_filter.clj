427b7be2fb248ba4f289bef0a787011f865c4138
Your Name <you@example.com>
2016-11-10 21:57:37 +0000

    Return nothing instead of nil

--- src/nodp/eip/message_filter.clj
+++ src/nodp/eip/message_filter.clj
@@ -2,6 +2,7 @@
   (:require [clojure.string :as str]
             [cats.builtin]
             [cats.core :as m]
+            [cats.monad.maybe :as maybe]
             [nodp.helpers :as helpers]
             [nodp.eip.helpers :as eip-helpers]))
 
@@ -16,14 +17,25 @@
 ;    ((m/curry 2 (helpers/flip comp)) :kind)
 ;    (m/curry 2 (helpers/flip str/starts-with?))))
 
+(defn- wrap-maybe
+  [expr]
+  (if (nil? expr)
+    (maybe/nothing)
+    (maybe/just expr)))
+
+(defmacro mif
+  [& more]
+  `(wrap-maybe (if ~@more)))
+
 (defn- make-handle-kind-items
   [kind]
   (fn [items]
-    (if (some (make-is-kind? kind) items)
-      (eip-helpers/handle-items items))))
+    (mif (some (make-is-kind? kind) items)
+         (eip-helpers/handle-items items))))
 
 (def handle-items-collection
-  (comp (partial remove nil?)
+  (comp (partial map m/join)
+        (partial remove maybe/nothing?)
         (partial m/<*> (map make-handle-kind-items ["ABC" "XYZ"]))))
 
 (defn- printall
