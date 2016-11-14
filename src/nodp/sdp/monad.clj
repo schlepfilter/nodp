69c5eca2539a724d7a70b4c6dbf4a79e1528c526
Your Name <you@example.com>
2016-11-14 05:42:22 +0000

    Print multiplication

--- src/nodp/sdp/monad.clj
+++ src/nodp/sdp/monad.clj
@@ -5,4 +5,15 @@
   [& vs]
   (apply helpers/ap * vs))
 
-(multiply [1 2 3 4] [5 6 7 8])
+(def prefix
+  (partial str "The result is: "))
+
+(def get-arguments
+  (comp prefix
+        multiply))
+
+(def print-result
+  (comp println
+        get-arguments))
+
+(print-result [1 2 3 4] [5 6 7 8])
