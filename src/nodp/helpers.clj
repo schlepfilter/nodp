49e00035fa3131fbd113a0ccc47b11817b37a835
Your Name <you@example.com>
2016-11-14 05:09:33 +0000

    Move curry

--- src/nodp/helpers.clj
+++ src/nodp/helpers.clj
@@ -1,6 +1,8 @@
 (ns nodp.helpers
   (:require [cats.monad.maybe :as maybe]
-            [riddley.walk :as riddley]))
+            [riddley.walk :as riddley]
+            [cats.builtin]
+            [cats.core :as m]))
 
 (defn flip
   [f]
@@ -25,6 +27,50 @@
   [operator & fs]
   `(comp (partial apply (functionize ~operator))
          (juxt ~@fs)))
+
+(defn wrap-maybe
+  [expr]
+  (if (nil? expr)
+    (maybe/nothing)
+    (maybe/just expr)))
+
+(defmacro maybe
+  [test then]
+  `(wrap-maybe (if ~test
+                 ~then)))
+
+(defmacro maybe-not
+  [test then]
+  `(wrap-maybe (if-not ~test
+                 ~then)))
+
+(defn ecurry
+  [arity f]
+  (fn [& outer-more]
+    (let [n (count outer-more)]
+      (if (== arity n)
+        (apply f outer-more)
+        (ecurry (- arity n)
+                (fn [& inner-more]
+                  (apply f (concat outer-more inner-more))))))))
+
+(defmacro curry
+  ([f]
+   `(m/curry ~f))
+  ([arity f]
+   `(ecurry ~arity ~f)))
+
+(defn ap
+  ([f x]
+   (m/<$> f x))
+  ([f x & more]
+   (apply m/<*>
+          (m/<$> (curry (-> more
+                            count
+                            inc)
+                        f)
+                 x)
+          more)))
 
 (defmacro defmulti-identity
   [mm-name]
@@ -62,18 +108,3 @@
   (comp (partial comp println)
         constantly))
 
-(defn wrap-maybe
-  [expr]
-  (if (nil? expr)
-    (maybe/nothing)
-    (maybe/just expr)))
-
-(defmacro maybe
-  [test then]
-  `(wrap-maybe (if ~test
-                 ~then)))
-
-(defmacro maybe-not
-  [test then]
-  `(wrap-maybe (if-not ~test
-                 ~then)))
