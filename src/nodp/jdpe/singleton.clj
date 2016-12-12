19d4b3973c06922ecf49b225255d9a75657e51cb
Your Name <you@example.com>
2016-12-12 20:17:00 +0000

    Use comp

--- src/nodp/jdpe/singleton.clj
+++ src/nodp/jdpe/singleton.clj
@@ -6,9 +6,9 @@
 (def get-next-serial
   (partial swap! generator inc))
 
-(defn print-next-serial
-  []
-  (println (get-next-serial)))
+(def print-next-serial
+  (comp println
+        get-next-serial))
 
 (dotimes [_ 3]
   (print-next-serial))
