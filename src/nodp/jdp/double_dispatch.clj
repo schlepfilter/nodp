19551790e94587a64ca6e4a68a311ecb6bbc4945
Your Name <you@example.com>
2016-12-24 05:01:21 +0000

    Get collisions

--- src/nodp/jdp/double_dispatch.clj
+++ src/nodp/jdp/double_dispatch.clj
@@ -1,40 +1,27 @@
 (ns nodp.jdp.double-dispatch
   (:require [clojure.math.combinatorics :as combo]
             [thi.ng.geom.core.intersect :as intersect]
-            [thi.ng.geom.rect :as rect]))
+            [thi.ng.geom.rect :as rect]
+            [nodp.helpers :as helpers]))
 
-(def asteroid
-  {:flaming  true
-   :position (rect/rect [0 0] [5 5])
-   :subject  :teroid})
+(def position-m
+  {:flaming-asteroid  (rect/rect [0 0] [5 5])
+   :space-station-iss (rect/rect [12 12] [14 14])
+   :space-station-mir (rect/rect [1 1] [2 2])
+   :meteroid          (rect/rect [10 10] [15 15])})
 
-(def mir
-  {:flaming  false
-   :position (rect/rect [1 1] [2 2])
-   :subject  :station})
+(def get-collisions
+  (comp (partial mapcat (comp combo/permutations
+                              (partial map first)))
+        (partial filter (comp (partial apply intersect/intersect-rect-rect?)
+                              (partial map last)))
+        (partial (helpers/flip combo/combinations) 2)))
 
-(def meteroid
-  {:flaming  false
-   :position (rect/rect [10 10] [15 15])
-   :subject  :teroid})
+(get-collisions position-m)
 
-(def iss
-  {:flmaing  false
-   :position (rect/rect [12 12] [14 14])
-   :subject  :station})
-
-(def bodies
-  #{asteroid mir meteroid iss})
-
-(def intersect?
-  (comp (partial apply intersect/intersect-rect-rect?)
-        (partial map :position)))
-
-;TODO implement collide with predicate dispatch when core.match supports predicate dispatch
+;TODO implement get-action with predicate dispatch when core.match supports predicate dispatch
 
 ;Road Map
 ;
 ;A good chunk of Maranget's algorithm for pattern matching has been implemented. We would like to flesh out the pattern matching functionality. Once that work is done, we'll move on to predicate dispatch.
 ;https://github.com/clojure/core.match/wiki/Overview
-
-(filter intersect? (combo/combinations bodies 2))
