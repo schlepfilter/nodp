22ba424a048b392d3ef3acf04aec05e3dffae8a3
Your Name <you@example.com>
2016-12-25 00:41:57 +0000

    Get rating

---
+++ src/nodp/hfdp/proxy.clj
@@ -0,0 +1,41 @@
+(ns nodp.hfdp.proxy
+  (:require [com.rpl.specter :as specter]
+            [incanter.distributions :as distributions]
+            [nodp.helpers :as helpers]))
+
+(defn- get-ratings-path
+  [object]
+  [(specter/must object) :ratings])
+
+;This definition is harder to read.
+;(def get-ratings-path
+;  (comp (partial (helpers/flip vector) :ratings) specter/must))
+
+(defn- make-set-rating
+  [{:keys [subject object rating]}]
+  (if (= subject object)
+    identity
+    (fn [person]
+      (specter/transform (get-ratings-path object)
+                         (partial (helpers/flip conj) rating)
+                         person))))
+
+(defn- get-rating
+  [{:keys [object person]}]
+  (->> (specter/select-one (get-ratings-path object) person)
+       distributions/mean
+       (str "Rating is ")))
+
+(defn- run-commands
+  [{:keys [commands person]}]
+  ((apply comp commands) person))
+
+(get-rating
+  {:object "Joe Javabean"
+   :person (run-commands
+             {:commands [(make-set-rating {:object "Joe Javabean"
+                                           :rating 3})
+                         (make-set-rating {:object  "Joe Javabean"
+                                           :subject "Joe Javabean"
+                                           :rating  10})]
+              :person   {"Joe Javabean" {:ratings [7]}}})})
