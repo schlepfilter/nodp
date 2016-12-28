29135c20d470229084a269b56e4f9e6598643ee1
Your Name <you@example.com>
2016-12-28 14:15:30 +0000

    Inspect

---
+++ src/nodp/jdpe/null_object.clj
@@ -0,0 +1,54 @@
+(ns nodp.jdpe.null-object
+  (:require [clojure.string :as str]
+            [com.rpl.specter :as specter]
+            [cuerdas.core :as cuerdas]
+            [nodp.helpers :as helpers]))
+
+(defn- make-get-turn-action
+  [on]
+  (comp (partial (helpers/flip str) (->> (if on
+                                           "ON"
+                                           "OFF")
+                                         (str " light ")))
+        str/capitalize
+        cuerdas/human))
+
+(defn make-add-action
+  [f]
+  (helpers/build (partial specter/transform* :actions)
+                 (comp (helpers/flip (helpers/curry 2 conj))
+                       f)
+                 identity))
+
+(defn- make-turn
+  [on]
+  (fn [{kind :kind :as environment}]
+    (if (= kind :null)
+      environment
+      (->> environment
+           (specter/setval :on on)
+           ((make-add-action (comp (make-get-turn-action on)
+                                   :kind)))))))
+
+
+(def check
+  (make-add-action :on))
+
+(def turn-on
+  (make-turn true))
+
+(def turn-off
+  (make-turn false))
+
+(def inspect
+  (comp :actions check turn-off turn-on))
+
+(defn- get-environment
+  [kind]
+  {:actions []
+   :kind    kind
+   :on      false})
+
+(map (comp inspect get-environment) [:oil-level
+                                     :brake-fluid
+                                     :null])
