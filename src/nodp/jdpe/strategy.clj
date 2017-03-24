64ed4e68fc7c2d52bcd0e2d9bffc8eed093d8a90
Your Name <you@example.com>
2017-03-24 08:39:50 +0000

    Use defcurried for make-change-speed

--- src/nodp/jdpe/strategy.clj
+++ src/nodp/jdpe/strategy.clj
@@ -1,16 +1,16 @@
 (ns nodp.jdpe.strategy
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [nodp.helpers :as helpers]))
 
-(defn make-change-speed
-  [gear]
-  (fn [speed]
-    (str "Working out correct gear at "
-         speed
-         "mph for a "
-         (-> gear
-             name
-             str/upper-case)
-         " gearbox")))
+(helpers/defcurried make-change-speed
+                    [gear speed]
+                    (str "Working out correct gear at "
+                         speed
+                         "mph for a "
+                         (-> gear
+                             name
+                             str/upper-case)
+                         " gearbox"))
 
 (def change-standard-speed
   (make-change-speed :standard))
