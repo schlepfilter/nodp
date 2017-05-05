2b00e1702751d4576e9209fd9f431a499468ecc3
Your Name <you@example.com>
2017-05-05 07:18:46 +0000

    call addEventListener for onresize event

--- src/nodp/helpers/window.cljs
+++ src/nodp/helpers/window.cljs
@@ -4,10 +4,15 @@
 
 (declare inner-height)
 
-(frp/register (fn []
-                (def resize
-                  (frp/event))
+(frp/register
+  (fn []
+    (def resize
+      (frp/event))
 
-                (def inner-height
-                  (frp/stepper js/window.innerHeight
-                               (helpers/<$> :inner-height resize)))))
+    (def inner-height
+      (frp/stepper js/window.innerHeight
+                   (helpers/<$> :inner-height resize)))
+
+    (js/addEventListener "resize"
+                         (fn []
+                           (resize {:inner-height js/window.innerHeight})))))
