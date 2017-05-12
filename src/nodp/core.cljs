24c28127ca23a57395928a96d646e1924e73c9b3
Your Name <you@example.com>
2017-05-12 11:26:23 +0000

    Render a list

--- src/nodp/core.cljs
+++ src/nodp/core.cljs
@@ -1,1 +1,12 @@
-(ns nodp.core)
+(ns nodp.core
+  (:require [reagent.core :as r]
+            [nodp.helpers.frp :as frp]
+            [nodp.helpers.location :as location]))
+
+(frp/restart)
+
+(frp/on (fn [pathname*]
+          (r/render [:ul [:li "absolute"]] (js/document.getElementById "app")))
+        location/pathname)
+
+(frp/activate)
