85963f42c1dc0f33ba8c36e92f9ba86aadc06cba
Your Name <you@example.com>
2017-05-13 04:45:12 +0000

    Extract app

--- src/nodp/core.cljs
+++ src/nodp/core.cljs
@@ -1,16 +1,20 @@
 (ns nodp.core
   (:require [reagent.core :as r]
+            [nodp.helpers :as helpers]
             [nodp.helpers.frp :as frp]
             [nodp.helpers.history :as history]
             [nodp.helpers.location :as location]))
 
 (frp/restart)
 
-(frp/on (fn [pathname*]
-          (r/render [:ul
-                     [:a {:on-click #(history/push-state {} {} "/absolute")}
-                      [:li "absolute"]]]
-                    (js/document.getElementById "app")))
-        location/pathname)
+(def app
+  (helpers/<$> (fn [pathname*]
+                 [:ul
+                  [:a {:on-click #(history/push-state {} {} "/absolute")}
+                   [:li "absolute"]]])
+               location/pathname))
+
+(frp/on (partial (helpers/flip r/render) (js/document.getElementById "app"))
+        app)
 
 (frp/activate)
