cd8efa6695e3fd3a7c8efd01bbcbb883e04fa928
Your Name <you@example.com>
2017-05-14 04:45:24 +0000

    Implement letter-count component

--- src/nodp/helpers/examples/letter_count.cljs
+++ src/nodp/helpers/examples/letter_count.cljs
@@ -1,4 +1,11 @@
 (ns nodp.helpers.examples.letter-count)
 
 (def letter-count
-  [:div])
+  [:div
+   [:h1 "Letter Counting Example"]
+   [:p "Example to show getting the current length of the input."]
+   [:div
+    [:p
+     "Text buffer: "
+     [:input]]
+    [:p "Start Typing!"]]])
