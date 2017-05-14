ceea070ec33506cf6f72c26b0468fc212588fe8e
Your Name <you@example.com>
2017-05-14 10:41:10 +0000

    Consolidate namespaces

--- src/nodp/core.cljs
+++ src/nodp/core.cljs
@@ -1,18 +1,19 @@
 (ns nodp.core
   (:require [bidi.bidi :as bidi]
             [reagent.core :as r]
-            [nodp.examples.letter-count :as letter-count]
             [nodp.helpers :as helpers]
             [nodp.helpers.frp :as frp]
             [nodp.helpers.history :as history]
             [nodp.helpers.location :as location]))
+
+(frp/restart)
 
 (def route
   ["/" {""                  :index
         "lettercount"       :letter-count
         "simpledatabinding" :simple-data-binding}])
 
-(def route-keys
+(def route-keywords
   [:letter-count :simple-data-binding])
 
 (defn example
@@ -24,18 +25,45 @@
    [:li path]])
 
 (def index
-  (->> route-keys
+  (->> route-keywords
        (map (comp example
                   (partial bidi/path-for route)))
        (cons :ul)
        (into [])))
 
+(def length
+  (frp/event))
+
+(defn letter-count-component
+  [message]
+  [:div
+   [:h1 "Letter Counting Example"]
+   [:p "Example to show getting the current length of the input."]
+   [:div [:p
+          "Text buffer: "
+          [:input {:on-change (fn [event*]
+                                (-> event*
+                                    .-target.value.length
+                                    length))}]]
+    [:p message]]])
+
+(def letter-count-behavior
+  (->> length
+       (helpers/<$> (partial str "length: "))
+       (frp/stepper "Start Typing!")
+       (helpers/<$> letter-count-component)))
+
+(def simple-data-binding
+  (frp/behavior [:div]))
+
 (def app
-  (helpers/=<< (comp {:index        (frp/behavior index)
-                      :letter-count letter-count/letter-count-behavior}
-                     :handler
-                     (partial bidi/match-route route))
-               location/pathname))
+  (helpers/=<<
+    (comp {:index               (frp/behavior index)
+           :letter-count        letter-count-behavior
+           :simple-data-binding simple-data-binding}
+          :handler
+          (partial bidi/match-route route))
+    location/pathname))
 
 (frp/on (partial (helpers/flip r/render) (js/document.getElementById "app"))
         app)
--- src/nodp/restart.cljs
+++
@@ -1,5 +0,0 @@
-(ns nodp.restart
-  (:require [nodp.helpers.frp :as frp]))
-
-(def restart
-  (frp/restart))
