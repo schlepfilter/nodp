c91911ff1a6baba9d113813e9de7c7788453b5c0
Your Name <you@example.com>
2017-05-15 10:37:35 +0000

    Define offset event

--- src/nodp/examples/drag_n_drop.cljs
+++ src/nodp/examples/drag_n_drop.cljs
@@ -5,15 +5,21 @@
 
 (def white "hsl(0, 0%, 100%)")
 
+(def offset
+  (frp/event))
+
 (def drag-n-drop-component
   [:div
-   [:div {:style {:background-image    "url(/img/logo.png)"
-                  :background-repeat   "no-repeat"
-                  :background-position "center"
-                  :background-color    black
-                  :color               white
-                  :height              200
-                  :width               200}}
+   [:div {:on-mouse-down (fn [event*]
+                           (offset {:x (.-nativeEvent.offsetX event*)
+                                    :y (.-nativeEvent.offsetY event*)}))
+          :style         {:background-image    "url(/img/logo.png)"
+                          :background-repeat   "no-repeat"
+                          :background-position "center"
+                          :background-color    black
+                          :color               white
+                          :height              200
+                          :width               200}}
     "Drag Me!"]
    [:h1 "Drag and Drop Example"]
    [:p "Example to show coordinating events to perform drag and drop"]])
