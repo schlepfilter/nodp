482afeeab0f8ce1e764af4db4528a2e705443e15
Your Name <you@example.com>
2017-05-15 10:34:34 +0000

    Make simple-data-binding-component lower order

--- src/nodp/examples/letter_count.cljs
+++ src/nodp/examples/letter_count.cljs
@@ -1,5 +1,5 @@
 (ns ^:figwheel-always nodp.examples.letter-count
-  (:require [nodp.helpers :as helpers :include-macros true]
+  (:require [nodp.helpers :as helpers]
             [nodp.helpers.frp :as frp]))
 
 
--- src/nodp/examples/simple_data_binding.cljs
+++ src/nodp/examples/simple_data_binding.cljs
@@ -1,5 +1,5 @@
 (ns ^:figwheel-always nodp.examples.simple-data-binding
-  (:require [nodp.helpers :as helpers :include-macros true]
+  (:require [nodp.helpers :as helpers]
             [nodp.helpers.frp :as frp]))
 
 (defn partial-name
@@ -12,23 +12,23 @@
                                event))
             :placeholder (str "Enter " label "...")}]])
 
-(helpers/defcurried simple-data-binding-component
-                    [{:keys [first-name last-name]} full-name]
-                    [:div
-                     [:h1 "Simple Data Binding Example"]
-                     [:p "Show simple concepts of data binding!"]
-                     [partial-name {:event first-name
-                                    :label "First Name"}]
-                     [partial-name {:event last-name
-                                    :label "Last Name"}]
-                     [:div "Full Name"]
-                     [:div full-name]])
-
 (def first-name
   (frp/event))
 
 (def last-name
   (frp/event))
+
+(defn simple-data-binding-component
+  [full-name]
+  [:div
+   [:h1 "Simple Data Binding Example"]
+   [:p "Show simple concepts of data binding!"]
+   [partial-name {:event first-name
+                  :label "First Name"}]
+   [partial-name {:event last-name
+                  :label "Last Name"}]
+   [:div "Full Name"]
+   [:div full-name]])
 
 (def full-name
   ((helpers/lift-a 3 str)
