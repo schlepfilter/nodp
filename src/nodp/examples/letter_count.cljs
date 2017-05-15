2a7f5fd93d9217621ec18d1f36072b1a716f411c
Your Name <you@example.com>
2017-05-15 10:32:54 +0000

    Make letter-count-component lower order

--- src/nodp/examples/letter_count.cljs
+++ src/nodp/examples/letter_count.cljs
@@ -2,24 +2,25 @@
   (:require [nodp.helpers :as helpers :include-macros true]
             [nodp.helpers.frp :as frp]))
 
-(helpers/defcurried letter-count-component
-                    [e message]
-                    [:div
-                     [:h1 "Letter Counting Example"]
-                     [:p "Example to show getting the current length of the input."]
-                     [:div [:p
-                            "Text buffer: "
-                            [:input {:on-change (fn [event*]
-                                                  (-> event*
-                                                      .-target.value.length
-                                                      e))}]]
-                      [:p message]]])
 
 (def length
   (frp/event))
+
+(defn letter-count-component
+  [message]
+  [:div
+   [:h1 "Letter Counting Example"]
+   [:p "Example to show getting the current length of the input."]
+   [:div [:p
+          "Text buffer: "
+          [:input {:on-change (fn [event*]
+                                (-> event*
+                                    .-target.value.length
+                                    length))}]]
+    [:p message]]])
 
 (def letter-count
   (->> length
        (helpers/<$> (partial str "length: "))
        (frp/stepper "Start Typing!")
-       (helpers/<$> (letter-count-component length))))
+       (helpers/<$> letter-count-component)))
