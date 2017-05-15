4d930e84fb941bb4f2831c9f02191d66f6390f81
Your Name <you@example.com>
2017-05-15 02:55:02 +0000

    Extract partial-name

--- src/nodp/examples/simple_data_binding.cljs
+++ src/nodp/examples/simple_data_binding.cljs
@@ -1,16 +1,24 @@
 (ns nodp.examples.simple-data-binding
   (:require [nodp.helpers :as helpers :include-macros true]))
 
+(defn partial-name
+  [{:keys [event label]}]
+  [:div
+   [:label label]
+   [:input {:on-change   (fn [event*]
+                           (-> event*
+                               .-target.value
+                               event))
+            :placeholder (str "Enter " label "...")}]])
+
 (helpers/defcurried simple-data-binding-component
                     [{:keys [first-name last-name]} full-name]
                     [:div
                      [:h1 "Simple Data Binding Example"]
                      [:p "Show simple concepts of data binding!"]
-                     [:label "First Name"]
-                     [:input {:on-change   (fn [event*]
-                                             (-> event*
-                                                 .-target.value
-                                                 first-name))
-                              :placeholder "Enter First Name..."}]
+                     [partial-name {:event first-name
+                                    :label "First Name"}]
+                     [partial-name {:event last-name
+                                    :label "Last Name"}]
                      [:div "Full Name"]
                      [:div full-name]])
