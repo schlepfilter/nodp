db3888bd43c02e568730549d359a0730c87e1561
Your Name <you@example.com>
2017-05-15 09:28:29 +0000

    Add h1

--- src/nodp/examples/drag_n_drop.cljs
+++ src/nodp/examples/drag_n_drop.cljs
@@ -2,7 +2,9 @@
   (:require [nodp.helpers.frp :as frp]))
 
 (def drag-n-drop-component
-  [:div])
+  [:div
+   [:h1 "Drag and Drop Example"]
+   [:p "Example to show coordinating events to perform drag and drop"]])
 
 (def drag-n-drop
   (frp/behavior drag-n-drop-component))
