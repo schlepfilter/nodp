9caac3b185402371a196727c163a191ebd957fa5
Your Name <you@example.com>
2017-05-15 09:34:23 +0000

    Show image to drag

--- src/nodp/examples/drag_n_drop.cljs
+++ src/nodp/examples/drag_n_drop.cljs
@@ -3,6 +3,11 @@
 
 (def drag-n-drop-component
   [:div
+   [:div {:style {:background-image  "url(/img/logo.png)"
+                  :background-repeat "no-repeat"
+                  :height            200
+                  :width             200}}
+    "Drag Me!"]
    [:h1 "Drag and Drop Example"]
    [:p "Example to show coordinating events to perform drag and drop"]])
 
