01809633c542812615510d00d9a105a00aa962dd
Your Name <you@example.com>
2017-05-15 09:38:53 +0000

    Add background color

--- src/nodp/examples/drag_n_drop.cljs
+++ src/nodp/examples/drag_n_drop.cljs
@@ -1,12 +1,19 @@
 (ns ^:figwheel-always nodp.examples.drag-n-drop
   (:require [nodp.helpers.frp :as frp]))
 
+(def black "hsl(0, 0%, 0%)")
+
+(def white "hsl(0, 0%, 100%)")
+
 (def drag-n-drop-component
   [:div
-   [:div {:style {:background-image  "url(/img/logo.png)"
-                  :background-repeat "no-repeat"
-                  :height            200
-                  :width             200}}
+   [:div {:style {:background-image    "url(/img/logo.png)"
+                  :background-repeat   "no-repeat"
+                  :background-position "center"
+                  :background-color    black
+                  :color               white
+                  :height              200
+                  :width               200}}
     "Drag Me!"]
    [:h1 "Drag and Drop Example"]
    [:p "Example to show coordinating events to perform drag and drop"]])
