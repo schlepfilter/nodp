8364215f44824f5f7c9b28ea8a7eb55c43b64691
Your Name <you@example.com>
2017-05-16 23:54:50 +0000

    Handle response of GET

--- src/nodp/examples/intro.cljs
+++ src/nodp/examples/intro.cljs
@@ -1,10 +1,17 @@
 (ns ^:figwheel-always nodp.examples.intro
-  (:require [nodp.helpers.frp :as frp]))
+  (:require [nodp.helpers.frp :as frp]
+            [ajax.core :refer [GET POST]]))
+
+(def response
+  (frp/event))
 
 (def intro
-  (frp/behavior [:div
-                 [:h2 "Who to follow"]
-                 [:a {:href     "#"
-                      :on-click (fn [event*]
-                                  (.preventDefault event*))}
-                  "refresh"]]))
+  (frp/behavior
+    [:div
+     [:h2 "Who to follow"]
+     [:a {:href     "#"
+          :on-click (fn [event*]
+                      (.preventDefault event*)
+                      (GET (str "https://api.github.com/users?since=" 0)
+                           {:handler response}))}
+      "refresh"]]))
