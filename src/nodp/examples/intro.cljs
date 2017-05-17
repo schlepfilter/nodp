3dab4fa1eddb201a839b029f5ad661f3478466ef
Your Name <you@example.com>
2017-05-17 01:15:06 +0000

    Define click-components

--- src/nodp/examples/intro.cljs
+++ src/nodp/examples/intro.cljs
@@ -1,19 +1,36 @@
 (ns ^:figwheel-always nodp.examples.intro
   (:require [clojure.walk :as walk]
             [ajax.core :refer [GET POST]]
-            [nodp.helpers.frp :as frp]))
+            [com.rpl.specter :as s]
+            [nodp.helpers.frp :as frp]
+            [nodp.helpers.unit :as unit]))
 
 (def response
   (frp/event))
 
+(def clicks
+  (repeatedly 3 frp/event))
+
+(def click-components
+  (map (fn [click]
+         [:li [:a {:href     "#"
+                   :on-click (fn [event*]
+                               (.preventDefault event*)
+                               (click unit/unit))}
+               "x"]])
+       clicks))
+
 (def intro
   (frp/behavior
-    [:div
-     [:h2 "Who to follow"]
-     [:a {:href     "#"
-          :on-click (fn [event*]
-                      (.preventDefault event*)
-                      (GET (str "https://api.github.com/users?since=" 0)
-                           {:handler (comp response
-                                           walk/keywordize-keys)}))}
-      "refresh"]]))
+    (s/setval
+      s/END
+      click-components
+      [:div
+       [:h2 "Who to follow"]
+       [:a {:href     "#"
+            :on-click (fn [event*]
+                        (.preventDefault event*)
+                        (GET (str "https://api.github.com/users?since=" 0)
+                             {:handler (comp response
+                                             walk/keywordize-keys)}))}
+        "refresh"]])))
