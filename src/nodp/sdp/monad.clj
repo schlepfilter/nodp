bd23ee9da5b447a8b7908df7e211e83f7d351383
Your Name <you@example.com>
2017-06-03 08:57:01 +0000

    Inline multiply

--- src/nodp/sdp/monad.clj
+++ src/nodp/sdp/monad.clj
@@ -2,15 +2,11 @@
   (:require [cats.builtin]
             [nodp.helpers :as helpers]))
 
-(defn- multiply
-  [& vs]
-  (apply (helpers/lift-a *) vs))
-
 (def prefix
   (partial str "The result is: "))
 
 (def get-results
   (comp prefix
-        multiply))
+        (helpers/lift-a *)))
 
 (get-results [1 2 3 4] [5 6 7 8])
